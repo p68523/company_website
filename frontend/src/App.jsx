59700226553fd1a7309dd306fd74754e67201a4c
p68523 <p68523@gmail.com>
2025-03-16 00:55:23 +0900

    2-2 게시판  작성

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,62 @@
 import "./App.css";
+
 import Footer from "./Components/Footer/Footer";
 import Navbar from "./Components/Navbar/Navbar";
 
-import { BrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
+import MainPage from "./Pages/MainPage/MainPage";
+import About from "./Pages/About/About";
+import Leadership from "./Pages/Leadership/Leadership";
+import Board from "./Pages/Board/Board";
+import Services from "./Pages/Services/Services";
+import Contact from "./Pages/Contact/Contact";
+
+function Layout() {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <MainPage />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/leadership",
+        element: <Leadership />,
+      },
+      {
+        path: "/board",
+        element: <Board />,
+      },
+      {
+        path: "/our-services",
+        element: <Services />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Footer />
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
